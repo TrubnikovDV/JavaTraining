c8696f49ff7f9aad7c01ec537ea936bb7511e476
Dmitriy <truq19990@gmail.com>
2022-05-16 14:36:13 +0400

    ver 2.0

--- app/src/main/java/JavaTraining/MyFirstProgram.java
+++ app/src/main/java/JavaTraining/MyFirstProgram.java
@@ -3,25 +3,14 @@ package JavaTraining;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        hello("world");
-        hello("Dima");
 
-        double len = 5;
-      System.out.println(len = area(len));
+      Square s = new Square(5);
+      System.out.println("Площадь квадрата " + "l" + " = " + s.area());
 
-      double a = 2;
-      double b=4;
-      System.out.println("Площадь прямоугольника " + "a" + " и " + "b" + " = " +  area (a,b));
-    }
-    public static void hello(String somebody){
+      Rectangle r = new Rectangle( 2,4);
+      System.out.println("Площадь прямоугольника " + "a" + " и " + "b" + " = " +  r.area());
 
-      System.out.println("Hello " + somebody);
-    }
-    public static double area (double l){
-      return l*l;
-    }
-    public static double area (double a, double b){
-      return a*b;
     }
     }
+
 
