b8a0b18ac21313b90e2c5ea708f90878744113ef
Dmitriy <truq19990@gmail.com>
2022-05-16 16:29:50 +0400

    ver 2.1
    добавили формулу вычисления расстояния между 2 точками в двумерном пространстве

--- app/src/main/java/JavaTraining/MyFirstProgram.java
+++ app/src/main/java/JavaTraining/MyFirstProgram.java
@@ -1,8 +1,14 @@
 
 package JavaTraining;
 
+import java.text.DecimalFormat;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
+
+      Point ab=new Point(1.0,5.0,1.0,5.0);
+      String result = new DecimalFormat("#0.00").format(ab.distance());
+      System.out.println("Расстояние между двумя точками AB = " + result);
 
       Square s = new Square(5);
       System.out.println("Площадь квадрата " + "l" + " = " + s.area());
---
+++ app/src/main/java/JavaTraining/Point.java
@@ -0,0 +1,20 @@
+package JavaTraining;
+
+public class Point {
+  public double xa;
+  public double xb;
+  public double ya;
+  public double yb;
+
+
+  public Point (double xa, double xb, double ya, double yb ){
+    this.xa=xa;
+    this.xb=xb;
+    this.ya=ya;
+    this.yb=yb;
+  }
+
+  public double distance (){
+    return  Math.sqrt(Math.pow((this.xb-this.xa),2)+Math.pow((this.yb - this.ya), 2));
+  }
+}
